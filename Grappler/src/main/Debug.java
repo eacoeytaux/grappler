22669469840f853567e92bd686a4659edebc6bd8
eacoeytaux <eacoeytaux@gmail.com>
2015-05-17 13:10:39 -0400

    Fixed bug with Debug class (oh the irony)

--- Grappler/src/main/Debug.java
+++ Grappler/src/main/Debug.java
@@ -7,9 +7,19 @@ public class Debug {
 	
 	public static HashMap<String, String> log = new HashMap<String, String>();
 	
-	public static void logMessage(String tag, String message) { //TODO in C++, make optional printMessage parameter
-		log.get(tag).concat(message);
-		if (DEBUG) System.out.println(message);
+	public static void logMessage(String tag, String message, boolean printMessage) { //TODO in C++, make optional printMessage parameter
+		message += "/n";
+		if (log.containsKey(tag)) {
+			log.get(tag).concat(message);
+		} else {
+			log.put(tag, message);
+		}
+		
+		if (printMessage) System.out.println(message);
+	}
+	
+	public static void logMessage(String tag, String message) {
+		logMessage(tag, message, DEBUG);
 	}
 	
 	public static void logMessage(String tag, int message) {
